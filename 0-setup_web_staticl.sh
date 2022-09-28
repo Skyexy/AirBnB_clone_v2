31210f396791968d1e902102aff22db93eeaa7b8
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-28 21:55:02 +0100

    Create 0-setup_web_staticl.sh

---
+++ 0-setup_web_staticl.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get install nginx
+mkdir –p data/web_static/releases/test/
+mkdir –p data/web_static/shared/
+code = 
+"
+	<html>
+		<head>
+		</head>
+		<body>
+			Holberton School
+		</body>
+	</html>
+"
+echo $code > data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R $USER:$USER /data/
