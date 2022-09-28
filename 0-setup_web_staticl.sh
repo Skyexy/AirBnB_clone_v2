3cd2848a86a303f1cc69e2a509dd825c3663af7f
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-28 22:21:07 +0100

    Update 0-setup_web_staticl.sh

--- 0-setup_web_staticl.sh
+++ 0-setup_web_staticl.sh
@@ -1,17 +1,12 @@
+#!/usr/bin/env bash
+# Write a Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get install nginx
+ufw allow 'Nginx HTTP'
 mkdir –p data/web_static/releases/test/
 mkdir –p data/web_static/shared/
-code = 
-"
-	<html>
-		<head>
-		</head>
-		<body>
-			Holberton School
-		</body>
-	</html>
-"
-echo $code > data/web_static/releases/test/index.html
+echo "Holberton School" > data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R $ubuntu:$ubuntu /data/
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+service nginx restart
