676834902715663c2f84342e7b7d160f64c605c7
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-29 20:53:34 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,7 +10,7 @@ from os.path import isfile
 from fabric.api import *
 
 env.user = 'ubuntu'
-env.hosts = ['35.196.121.183', '35.196.239.45']
+env.hosts = ['3.237.48.129', '3.236.106.108']
 
 
 def do_pack():
@@ -29,18 +29,18 @@ def do_deploy(archive_path):
     """ Distribute an archive to the web servers """
     if not isfile(archive_path):
         return False
-    put(archive_path, '/tmp/')
-    archive = archive_path.replace('.tgz', '')
-    archive = archive.replace('versions/', '')
-    run('mkdir -p /data/web_static/releases/{}/'.format(archive))
-    run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-        .format(archive, archive))
-    run('rm /tmp/{}.tgz'.format(archive))
-    run('mv /data/web_static/releases/{}/web_static/* '.format(archive) +
-        '/data/web_static/releases/{}/'.format(archive))
-    run('rm -rf /data/web_static/releases/{}/web_static'.format(archive))
-    run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-        .format(archive))
-    print('New version deployed!')
-    return True
+    try:
+        put(archive_path, '/tmp/')
+        archive = archive_path.replace('.tgz', '')
+        archive = archive.replace('versions/', '')
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+            .format(archive, archive))
+        run('rm /tmp/{}.tgz'.format(archive))
+        run('rm -rf /data/web_static/current')
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive))
+        return True
+    except:
+        return False
