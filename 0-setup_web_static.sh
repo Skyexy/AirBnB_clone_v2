b5def918ca8258fc9d274f659b9aba5fd80edf26
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-30 17:39:39 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,8 @@
 sudo apt-get update
 sudo apt-get install nginx
 ufw allow 'Nginx HTTP'
-mkdir –p data/web_static/releases/test/
-mkdir –p data/web_static/shared/
-echo "Holberton School" > data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R $ubuntu:$ubuntu /data/
 sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
