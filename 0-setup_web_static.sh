fc9d69c2280edaed8d843159c5086a823a5d4b3d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-30 17:51:46 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,6 @@
 # Write a Bash script that sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "I'm at MAGFest 2020" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
