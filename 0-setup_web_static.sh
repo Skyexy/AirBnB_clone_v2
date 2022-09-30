f97ba23ee6b500640b1263564e2533e42d80aa15
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-30 18:05:36 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,5 +7,5 @@ mkdir -p /data/web_static/releases/test /data/web_static/shared
 echo "I'm at MAGFest 2020" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sed -i '/listen 80 default_server;/a \t location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
