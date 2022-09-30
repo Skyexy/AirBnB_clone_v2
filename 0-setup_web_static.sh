3ee76d38190d1b6222b240f5250b72a3b1d2774d
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-09-30 17:42:14 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,8 +4,8 @@ apt-get -y update
 apt-get -y install nginx
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R $ubuntu:$ubuntu /data/
+echo "I'm at MAGFest 2020" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
 sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 service nginx restart
