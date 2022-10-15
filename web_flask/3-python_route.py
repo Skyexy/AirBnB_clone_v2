3270c775249eb8aea1f870a183d0684ade3fa296
Skyexy <99495685+Skyexy@users.noreply.github.com>
2022-10-15 10:52:48 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,7 +23,7 @@ def c(text):
     """ Print C <text> """
     return 'C {}'.format(text).replace('_', ' ')
 
-
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """ Print Python <text> """
